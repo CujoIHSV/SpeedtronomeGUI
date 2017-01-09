0bd940599eb09499b58643217ff27558f7f9e829
CujoIHSV <cujoihsv@gmail.com>
2017-01-09 02:31:03 -0700

    Initial commit. Imported Metronome.cs from CLI version. Not working.

---
+++ SpeedtronomeGUI/Metronome.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Speedtronome
+{
+    class Metronome
+    {
+        private double period;      // Metronome period (in ms)
+        private SoundPlayer tone;   // The .wav to play for the tick
+
+        private static readonly SoundPlayer defaultTone = new SoundPlayer("2.wav");
+
+        public Metronome(double bpm)
+        {
+            period = 60.0 * 1000 / bpm;
+            tone = defaultTone; // DEFAULT ONLY
+            tone.Load();
+        }
+
+        public Metronome(int frames)
+        {
+            period = 1000 * frames / 60.098814;
+            tone = defaultTone; // DEFAULT ONLY
+            tone.Load();
+        }
+
+        public void Oscillate()
+        {
+            Timer timer = new Timer(Tick, null, 0, (int)period);
+        }
+
+        private void Tick(object _)
+        {
+            tone.Play();
+        }
+    }
+}
